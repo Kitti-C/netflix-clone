43c5fce5229c130bccf54378f45dd62a5e3270b4
KittiC <svc8zzzz@hotmail.com>
2022-08-17 09:37:09 +0700

    Update MoviesRow

--- components/Movies.tsx
+++ components/Movies.tsx
@@ -28,18 +28,18 @@ function Movies({ title, movies }: Props) {
   }
 
   return (
-    <div className="h-40 space-y-0.5 md:space-y-2 ">
+    <div className="h-40  space-y-0.5 md:space-y-2 ">
       <h1 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
         {title}
       </h1>
-      <div className="relative group md:-ml-2">
+      <div className="relative group md:-ml-2 ">
         <ChevronLeftIcon
           onClick={() => handleClick('left')}
           className={`absolute top-0 bottom-0 left-2  z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100`}
         />
         <div
           ref={rowRef}
-          className="flex items-center space-x-1 overflow-x-scroll scrollbar-hide md:space-x-2 md:p-2"
+          className="flex h-32 md:h-40  items-center space-x-1 overflow-x-scroll scrollbar-hide md:space-x-2 md:p-2"
         >
           {movies.map((movie) => (
             <Thumbnail key={movie.id} movie={movie} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -59,7 +59,7 @@ const Home = ({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className="relative  pl-4 pb-24 lg:space-y-24 lg:pl-16">
+      <main className="relative  px-4 pb-24 lg:space-y-24 lg:px-16">
         <Banner netflixOriginals={netflixOriginals} />
         <section className="md:space-y-24 ">
           <Movies title="Trending Now" movies={trendingNow} />
